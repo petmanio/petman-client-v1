c64615f4e85a14ec3c533d9d63ad5e5e76a7b116
andantonyan <andantonyan@gmail.com>
2017-02-28 13:41:43 +0400

    feat(environment): http -> https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'http://walkypet-api.herokuapp.com',
+  apiEndpoint: 'https://walkypet-api.herokuapp.com',
   fb: {
     appId: '1352754344791293',
     scope: 'public_profile,email'
