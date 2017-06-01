4bcb88b1042f637281cacc840ed7ada21ea84125
andantonyan <andantonyan@gmail.com>
2017-06-01 23:00:57 +0400

    feat(translation): about us

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -6,10 +6,22 @@ export interface IAboutUpComponent {
 @Component({
   selector: 'app-about-us',
   template: `
-   
+    <div class="columns">
+      <div class="column">
+        <div class="pm-about-us-content">
+          <span class="pm-font-20 pm-color-gray">{{'about_us_content' | translate}}</span>
+        </div>
+      </div>
+    </div>
   `,
   styles: [`
-   
+    .pm-about-us-content {
+      margin-top: 20%;
+      width: 70%;
+      text-align: justify;
+      margin-left: auto;
+      margin-right: auto;
+    }
   `]
 })
 export class AboutUsComponent implements IAboutUpComponent {
